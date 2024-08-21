baee35d3b3d12eece65ddcddf8896cbb4682b0b1
jiyoungk-0 <jiyoungk0929@gmail.com>
2024-08-20 20:27:36 -0500

    Update QR Code Generator.py

--- QR Code Generator.py
+++ QR Code Generator.py
@@ -1,9 +1,10 @@
 import qrcode
 
-data = 'Don\'t forget to subscribe'
+# URL to be encoded in the QR code
+url = 'https://www.google.com'
 
 # Generate QR code image
-img = qrcode.make(data)
+img = qrcode.make(url)
 
 # Define file path to save QR code image
 img.save('~/QRcode/qrcode.png')
